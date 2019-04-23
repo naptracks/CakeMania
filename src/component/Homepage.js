4736ea9bcc898d41153dd89fa29ba7e4bfe316ab
Vermaut Hélène <helene.vermaut@gmail.com>
2019-04-23 11:46:01 +0200

    Maj carousel - Maj margin bbody - Maj Homepahe

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Carousel from './homepage/Carousel';
+import Recommandation from './homepage/Recommandation';
 
 class Homepage extends Component {
     render() {
         return (
             <div>
-                <Carousel />                
+                <Carousel />  
+                <Recommandation />              
             </div>
         );
     }
