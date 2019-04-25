a7510be4e967ef26726d143560b2ee5853658a54
maxVand <m.vandine@live.fr>
2019-04-25 16:13:54 +0200

    Fonction update dans mon compte en cliqunt sur le nom déclenche modal

--- src/component/moncompte/InfoBox.js
+++ src/component/moncompte/InfoBox.js
@@ -4,31 +4,13 @@ class InfoBox extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            name: this.props.boxText1,
-            userNameInput :''
-        }
+        
         
         
         
     }
 
-inputChange = (e)=>{
-    this.setState({
-        userNameInput : e.target.value
-    })
-     return this.state.userNameInput;
-}
 
-userNameSubmit = (e)=>{
-    e.preventDefault();
-    this.setState({
-        name : this.state.userNameInput
-    })
-    console.log(this.state.userNameInput);
-    console.log(this.state.name);
-
-}
 
 
 
@@ -54,39 +36,8 @@ userNameSubmit = (e)=>{
                     <p className="card-text ">{this.props.boxText5}</p>
 
                 </div>
-
-                 {/***********************MODAL FORMULAIRE NOM**********/}
-                <div id="mymodal" className="modal" tabindex="-1" role="dialog">
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">Modifier</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <p>Saisir votre nom svp </p>
-                                <form onSubmit={this.userNameSubmit}>
-                                <label htmlFor="formName">Nouveau nom:</label> <br/>
-                                <input 
-                                value={this.state.userNameInput} 
-                                type="text" 
-                                id="formName"
-                                onChange={this.inputChange}
-                                />
-                                 <br/>
-                                <div className="modal-footer">
-                                
-                                <button type="submit" className="btn btn-primary">Valider</button>
-                            </div>
-                                </form>
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
+</div>
+                
         );
     }
 }
