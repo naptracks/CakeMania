a281fdbcff35d2015544805cf13b0ee97316737e
Vermaut Hélène <helene.vermaut@gmail.com>
2019-04-25 16:59:34 +0200

    Admin modal pour l'ajout d'un gateau

--- src/component/admin/GestionProduits.js
+++ src/component/admin/GestionProduits.js
@@ -59,11 +59,73 @@ class GestionProduits extends Component {
                     <div className="col-4 card" id="creationProduit" data-toggle="modal" data-target="#add">
                         <img src={imgNewProduct} className="card-img-top" alt="image gestion client" />
                         <div className="card-body text-center">
-                            <i class="fas fa-plus-circle"></i>
+                            <i className="fas fa-plus-circle"></i>
                             <hr />
                             <p>Ajouter un produit</p>
                         </div>
                     </div>
+                    {/* MODAL d'ajout d'un produit */}
+                    <div id="add" className="modal" tabindex="-1" role="dialog">
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">Ajouter un produit</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <form>
+                                        <div className="form-group">
+                                            <label htmlFor="name">Nom du produit</label>
+                                            <input type="text" id="name" className="form-control" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="description">Description</label>
+                                            <input type="text" id="description" className="form-control" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="price">Prix (mettre '.' à la place des virgules)</label>
+                                            <input type="text" id="price" className="form-control" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="image">Mettre l'url de l'image</label>
+                                            <input type="text" id="image" className="form-control" />
+                                        </div>
+                                        {/* BTN radio true false pour l'affichage en première page */}
+                                        <div className="form-group">
+                                            <label htmlFor="image">Le produit doit-il être sur la page d'accueil ?</label>
+                                            <div className="form-check form-check-inline">
+                                                <input className="form-check-input" type="radio" id="true" value="option1" />
+                                                <label className="form-check-label" htmlFor="true">Oui</label>
+                                            </div>
+                                            <div className="form-check form-check-inline">
+                                                <input className="form-check-input" type="radio" id="false" value="option2" />
+                                                <label className="form-check-label" htmlFor="false">Non</label>
+                                            </div>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="size">Nombre de parts</label>
+                                            <input type="text" id="size" className="form-control" />
+                                        </div>
+                                        <div class="form-group">
+                                            <label htmlFor="base">Base du gâteau</label>
+                                            <select class="form-control" id="base">
+                                                <option>Vanille</option>
+                                                <option>Chocolat</option>
+                                                <option>Crème patissière</option>
+                                            </select>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                                    <button type="button" className="btn btn-primary">Ajouter</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Fin MODAL d'ajout d'un produit */}
                     <div id="toutNosProduits" className="row">
                         {this.affichageProduits()}
                     </div>
