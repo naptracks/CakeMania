08a4d6ac61af9eb04f1fd6bfaf57a1eb9215bcb4
Vermaut Hélène <helene.vermaut@gmail.com>
2019-04-29 11:33:32 +0200

    Début de création du panier

--- src/component/panier/Panier.js
+++ src/component/panier/Panier.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class Panier extends Component {
+    state = {
+        tabPanier: []
+    }
+
+    affichageTabPanier = () => {
+        return this.state.tabPanier.map((item) =>
+            <tr>
+                <td>Mon article</td>
+                <td>Ma quantité</td>
+                <td>le prix unitaire</td>
+                <td>le prix total</td>
+            </tr>
+        );
+    }
+
     render() {
         return (
-            <div className="marginDeBase">
-                Gestion du panier
+            <div className="marginDeBase container">
+                <h2>Mon panier</h2>
+                <hr />
+                <p>Retrouvez ici tous vos articles sélectionnés</p>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Article</th>
+                            <th>Quantité</th>
+                            <th>Prix unitaire</th>
+                            <th>Prix total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.affichageTabPanier()}
+                    </tbody>
+                </table>
             </div>
         );
     }
